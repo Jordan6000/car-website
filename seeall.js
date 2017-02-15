89bd0e9c00d3b03c3a6e9a0d2666177cfbf5afe6
Jordan6000 <8979@esher.ac.uk>
2017-02-15 16:59:53 +0000

    seeallpictures

--- seeall.js
+++ seeall.js
@@ -1,21 +1,21 @@
 console.log('seeall')
 
 var cars = [
-  {name: "LAMBORGHINI " , imageUrl: "Seeall/aventador.jpg" , topspeed: " Top speed: 220mph"},
-  {name: "FERRARI F12", imageUrl: "Seeall/ferrari.jpg"},
-  {name: "BUGATTI VEYRON", imageUrl: "Seeall/bugatti.jpg"},
-  {name: "FERRARI LEFERRARI", imageUrl: "Seeall/leferrari.jpg"},
-  {name: "LAMBORGHINI VENENO", imageUrl: "Seeall/veneno.jpg"},
-  {name: "BMW M7", imageUrl: "Seeall/m7.jpg"},
-  {name: "BMW M3", imageUrl: "Seeall/m3.jpg"},
-  {name: "PAGANI", imageUrl: "Seeall/pagani.jpg"},
-  {name: "AUDI A6", imageUrl: "Seeall/a6.jpg"},
-  {name: "MERCEDES SLS", imageUrl: "Seeall/sls.jpg"},
-  {name: "TESLA MODEL S", imageUrl: "Seeall/tesla.jpg"},
-  {name: "MCLAREN P1", imageUrl: "Seeall/mclarenp1.jpg"},
-  {name: "AUDI TT", imageUrl: "Seeall/tt.jpg"},
-  {name: "BENTLEY CONTINENTAL GT", imageUrl: "Seeall/bentley.jpg"},
-  {name: "PORSCHE 911", imageUrl: "Seeall/porsche.jpg"}
+  {name: "LAMBORGHINI AVENTADOR" , imageUrl: "Seeall/aventador.jpg" , topspeed: " Top speed: 220mph ", price: " Price(£): 180,000.00"},
+  {name: "FERRARI F12", imageUrl: "Seeall/ferrari.jpg" , topspeed: " Top speed: 210mph " , price: " Price(£): 160,000.00"},
+  {name: "BUGATTI VEYRON", imageUrl: "Seeall/bugatti.jpg" , topspeed: " Top speed: 230mph ", price: " Price(£): 210,000.00"},
+  {name: "FERRARI LEFERRARI", imageUrl: "Seeall/leferrari.jpg" ,topspeed: " Top speed: 225mph " , price: " Price(£): 190,000.00"},
+  {name: "LAMBORGHINI VENENO", imageUrl: "Seeall/veneno.jpg" ,topspeed: " Top speed: 2300mph " , price: " Price(£): 230,000.00"},
+  {name: "BMW M7", imageUrl: "Seeall/m7.jpg" , topspeed: " Top speed: 205mph " , price: " Price(£): 90,000.00"},
+  {name: "BMW M3", imageUrl: "Seeall/m3.jpg" , topspeed: " Top speed: 195mph " , price: " Price(£): 55,000.00"},
+  {name: "PAGANI HUAYRA", imageUrl: "Seeall/pagani.jpg" , topspeed: " Top speed: 210mph " , price: " Price(£): 140,000.00"},
+  {name: "AUDI A6", imageUrl: "Seeall/a6.jpg" , topspeed: " Top speed: 190mph " , price: " Price(£): 60,000.00"},
+  {name: "MERCEDES SLS", imageUrl: "Seeall/sls.jpg" , topspeed: " Top speed: 200mph " , price: " Price(£): 100,000.00"},
+  {name: "TESLA MODEL S", imageUrl: "Seeall/tesla.jpg" , topspeed: " Top speed: 185mph " , price: " Price(£): 120,000.00"},
+  {name: "MCLAREN P1", imageUrl: "Seeall/mclarenp1.jpg" , topspeed: " Top speed: 210mph " , price: " Price(£): 210,000.00"},
+  {name: "AUDI TT", imageUrl: "Seeall/tt.jpg" , topspeed: " Top speed: 180mph " , price: " Price(£): 60,000.00"},
+  {name: "BENTLEY CONTINENTAL GT", imageUrl: "Seeall/bentley.jpg" , topspeed: " Top speed: 215mph " , price: " Price(£): 240,000.00"},
+  {name: "PORSCHE 911", imageUrl: "Seeall/porsche.jpg" , topspeed: " Top speed: 225mph " , price: " Price(£): 190,000.00"}
 
 ]
 var carselement = $('.cars');
@@ -34,15 +34,15 @@ cars.forEach(function(car) {
   var carelement = $('<figure class="col-sm-4 car">');
   var titleelement = $('<p class="sansserif">' + car.name + '</p>');
   var imageelement = $('<img class="carimage" src="' + car.imageUrl + '" alt="car" />' );
-  var topspeed = $('<p class="sansserif">' + car.topspeed + '</p>');
-  carelement.append(titleelement,imageelement,topspeed);
+  carelement.append(titleelement,imageelement);
   carselement.append(carelement);
 
   carelement.click(function() {
     console.log("You clicked");
     modal.css("display" , "block");
     modalimage.attr("src" , car.imageUrl);
-    captionText.text(car.name + car.topspeed);
+    captionText.text(car.name);
+    captionText.text(car.topspeed + car.price);
   });
 
 });
